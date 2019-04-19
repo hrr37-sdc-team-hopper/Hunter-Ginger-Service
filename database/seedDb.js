b1aac47e5bac5e87c3eda609a14c86ed1547216c
hunterxdong <hunterdong123@gmail.com>
2019-04-19 16:52:53 -0700

    refactored seedDB to generate based on books, not authors

--- database/seedDb.js
+++ database/seedDb.js
@@ -5,27 +5,25 @@ const profilePic = "http://d37pd3xfja253q.cloudfront.net/authors/sample-image-";
 const status = "Want to Read";
 
 //there are only three author pictures and they have the url ending in 1,2 or 3
-const createAuthor = () => {
-  var authorData = {
-    name: faker.name.findName(),
-    details: faker.lorem.paragraphs(),
-    profile_pic: `${profilePic + faker.random.number({ min: 1, max: 3 })}.jpg`,
-    followers: faker.random.number({ min: 0, max: 20000 }),
-    books: []
+const createBook = () => {
+  var bookData = {
+    title: faker.commerce.productName(),
+    description: faker.lorem.paragraphs(),
+    author_id: faker.random.number({ min: 1, max: 25 }),
+    published_year: faker.random.number({ min: 1920, max: 2019 }),
+    cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
+    status: status,
+    author: {
+      name: faker.name.findName(),
+      details: faker.lorem.paragraphs(),
+      profile_pic: `${profilePic +
+        faker.random.number({ min: 1, max: 3 })}.jpg`,
+      followers: faker.random.number({ min: 0, max: 20000 })
+    }
   };
-  for (var i = 0; i < 16; i++) {
-    var bookData = {
-      title: faker.commerce.productName(),
-      description: faker.lorem.paragraphs(),
-      author_id: faker.random.number({ min: 1, max: 25 }),
-      published_year: faker.random.number({ min: 1920, max: 2019 }),
-      cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
-      status: status
-    };
-    authorData.books.push(bookData);
-  }
-  return authorData;
+  return bookData;
 };
+console.log(createBook());
 //write fswait or query postgresql and query mongodb
 //data.author.name is the same as faker.random.number
 //16 books to author
