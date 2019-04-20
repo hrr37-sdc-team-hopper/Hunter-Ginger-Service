9d301321035e126ab225e24942f19a5c32c9d38e
hunterxdong <hunterdong123@gmail.com>
2019-04-19 21:08:34 -0700

    added csvWriter to help data generation

--- database/seedDb.js
+++ database/seedDb.js
@@ -1,29 +1,33 @@
 const faker = require("faker");
 const fs = require("file-system");
+const csvWriter = require("csv-write-stream");
+const writer = csvWriter();
 const coverPicture = "http://d37pd3xfja253q.cloudfront.net/books/sample-image-";
 const profilePic = "http://d37pd3xfja253q.cloudfront.net/authors/sample-image-";
 const status = "Want to Read";
 
-//there are only three author pictures and they have the url ending in 1,2 or 3
-const createBook = () => {
-  var bookData = {
-    title: faker.commerce.productName(),
-    description: faker.lorem.paragraphs(),
-    author_id: faker.random.number({ min: 1, max: 25 }),
-    published_year: faker.random.number({ min: 1920, max: 2019 }),
-    cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
-    status: status,
-    author: {
-      name: faker.name.findName(),
-      details: faker.lorem.paragraphs(),
-      profile_pic: `${profilePic +
-        faker.random.number({ min: 1, max: 3 })}.jpg`,
-      followers: faker.random.number({ min: 0, max: 20000 })
-    }
-  };
-  return bookData;
+var createBook = function() {
+  writer.pipe(fs.createWriteStream("author.csv"));
+  for (var i = 0; i < 10000000; i++) {
+    writer.write(
+      JSON.stringify({
+        title: faker.commerce.productName(),
+        description: faker.lorem.paragraphs(),
+        author_id: faker.random.number({ min: 1, max: 25 }),
+        published_year: faker.random.number({ min: 1920, max: 2019 }),
+        cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
+        status: status,
+        author: {
+          name: faker.name.findName(),
+          details: faker.lorem.paragraphs(),
+          profile_pic: `${profilePic +
+            faker.random.number({ min: 1, max: 3 })}.jpg`,
+          followers: faker.random.number({ min: 0, max: 20000 })
+        }
+      })
+    );
+  }
+  writer.end();
 };
-console.log(createBook());
-//write fswait or query postgresql and query mongodb
-//data.author.name is the same as faker.random.number
-//16 books to author
+
+createBook();
