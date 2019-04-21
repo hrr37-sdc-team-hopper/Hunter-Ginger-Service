f3f2757b8b953cd16b22d3b9b28f6007e1f15b61
hunterxdong <hunterdong123@gmail.com>
2019-04-20 17:35:40 -0700

    creates 10 million files, but uses over 20gb of ram

--- database/seedDb.js
+++ database/seedDb.js
@@ -8,11 +8,31 @@ const status = "Want to Read";
 
 //reffer to https://www.npmjs.com/package/csv-write-stream
 
-var createBook = function() {
-  writer.pipe(fs.createWriteStream("author.csv"));
-  for (var i = 0; i < 10000000; i++) {
-    writer.write(
-      JSON.stringify({
+// var createBook = function() {
+//   var bookData = {
+//     title: faker.commerce.productName(),
+//     description: faker.lorem.paragraphs(),
+//     author_id: faker.random.number({ min: 1, max: 25 }),
+//     published_year: faker.random.number({ min: 1920, max: 2019 }),
+//     cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
+//     status: status,
+//     author: {
+//       name: faker.name.findName(),
+//       details: faker.lorem.paragraphs(),
+//       profile_pic: `${profilePic +
+//         faker.random.number({ min: 1, max: 3 })}.jpg`,
+//       followers: faker.random.number({ min: 0, max: 20000 })
+//     }
+//   };
+//   return bookData
+// };
+
+for (var j = 0; j < 10; j++) {
+  var createBook = function() {
+    console.time("timing seed");
+    writer.pipe(fs.createWriteStream(`./data/${j}author.csv`));
+    for (var i = 0; i < 1000000; i++) {
+      writer.write({
         title: faker.commerce.productName(),
         description: faker.lorem.paragraphs(),
         author_id: faker.random.number({ min: 1, max: 25 }),
@@ -26,10 +46,15 @@ var createBook = function() {
             faker.random.number({ min: 1, max: 3 })}.jpg`,
           followers: faker.random.number({ min: 0, max: 20000 })
         }
-      })
-    );
-  }
-  writer.end();
-};
+      });
+    }
+    console.timeEnd("timing seed");
+  };
+  createBook();
+}
+writer.end();
 
-createBook();
+//end of the stream you close the connection.
+//if you are writing the stream, you are writing in chunks. that ends the connection and that
+//csv separates with commas.
+//one million is no problem but you need it to run 10 times.
