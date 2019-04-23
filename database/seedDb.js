5d7a8517a9b9998c27ad81774a7bc1c32338c1dd
hunterxdong <hunterdong123@gmail.com>
2019-04-22 19:35:43 -0700

    modified seed db file, made plans for seedingPostgres

--- database/seedDb.js
+++ database/seedDb.js
@@ -8,51 +8,72 @@ const status = "Want to Read";
 
 //reffer to https://www.npmjs.com/package/csv-write-stream
 
-// var createBook = function() {
-//   var bookData = {
-//     title: faker.commerce.productName(),
-//     description: faker.lorem.paragraphs(),
-//     author_id: faker.random.number({ min: 1, max: 25 }),
-//     published_year: faker.random.number({ min: 1920, max: 2019 }),
-//     cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
-//     status: status,
-//     author: {
-//       name: faker.name.findName(),
-//       details: faker.lorem.paragraphs(),
-//       profile_pic: `${profilePic +
-//         faker.random.number({ min: 1, max: 3 })}.jpg`,
-//       followers: faker.random.number({ min: 0, max: 20000 })
-//     }
-//   };
-//   return bookData
-// };
+var createBook = function() {
+  var bookData = {
+    title: faker.commerce.productName(),
+    description: faker.lorem.paragraphs(),
+    author_id: faker.random.number({ min: 1, max: 25 }),
+    published_year: faker.random.number({ min: 1920, max: 2019 }),
+    cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
+    status: status
+  };
+  return bookData;
+};
 
-for (var j = 0; j < 10; j++) {
-  var createBook = function() {
+var createAuthors = function() {
+  var authorData = {
+    name: faker.name.findName(),
+    details: faker.lorem.paragraphs(),
+    profile_pic: `${profilePic + faker.random.number({ min: 1, max: 3 })}.jpg`,
+    followers: faker.random.number({ min: 0, max: 20000 })
+  };
+  return authorData;
+};
+
+var makeBooks = function() {
+  var stream = fs.createWriteStream(`./data/books.csv`);
+  writer.pipe(stream);
+  var generateBooks = function() {
     console.time("timing seed");
-    writer.pipe(fs.createWriteStream(`./data/${j}author.csv`));
-    for (var i = 0; i < 1000000; i++) {
-      writer.write({
-        title: faker.commerce.productName(),
-        description: faker.lorem.paragraphs(),
-        author_id: faker.random.number({ min: 1, max: 25 }),
-        published_year: faker.random.number({ min: 1920, max: 2019 }),
-        cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
-        status: status,
-        author: {
-          name: faker.name.findName(),
-          details: faker.lorem.paragraphs(),
-          profile_pic: `${profilePic +
-            faker.random.number({ min: 1, max: 3 })}.jpg`,
-          followers: faker.random.number({ min: 0, max: 20000 })
-        }
-      });
+    for (var i = 0; i < 10000000; i++) {
+      var books = createBook();
+      writer.write(books);
     }
     console.timeEnd("timing seed");
   };
-  createBook();
-}
-writer.end();
+  generateBooks();
+};
+
+makeBooks();
+
+//fs write closes the stream
+//drain maybe later
+//backpressure
+
+// var createBook = function () {
+//   console.time("timing seed");
+// fs.createWriteStream(`./data/${j}author.csv`));
+//   for (var i = 0; i < 10000000; i++) {
+//     writer.write({
+//       title: faker.commerce.productName(),
+//       description: faker.lorem.paragraphs(),
+//       author_id: faker.random.number({ min: 1, max: 25 }),
+//       published_year: faker.random.number({ min: 1920, max: 2019 }),
+//       cover: coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg",
+//       status: status,
+//       author: {
+//         name: faker.name.findName(),
+//         details: faker.lorem.paragraphs(),
+//         profile_pic: `${profilePic +
+//           faker.random.number({ min: 1, max: 3 })}.jpg`,
+//         followers: faker.random.number({ min: 0, max: 20000 })
+//       }
+//     });
+//   }
+//   console.timeEnd("timing seed");
+// };
+
+//run with node--max - old - space - size=20480 database / seedDB.js
 
 //end of the stream you close the connection.
 //if you are writing the stream, you are writing in chunks. that ends the connection and that
