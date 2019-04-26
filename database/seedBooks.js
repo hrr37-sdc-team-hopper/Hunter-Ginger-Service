783cdfbb534950d0a6f74d53e95fee81feb1a44b
hunterxdong <hunterdong123@gmail.com>
2019-04-25 22:47:16 -0700

    cleaned up seedBooks code

--- database/seedBooks.js
+++ database/seedBooks.js
@@ -10,7 +10,7 @@ stream.write("title,description,author_id,published_year,cover,status\n");
 
 function makeBooks(writer, callback) {
   console.time("timing seed");
-  var i = 100; //10000000;
+  var i = 10000000;
   (function write() {
     var ok = true;
     while (i > 0 && ok) {
@@ -21,7 +21,6 @@ function makeBooks(writer, callback) {
       var published_year = faker.random.number({ min: 1920, max: 2019 });
       var cover =
         coverPicture + faker.random.number({ min: 1, max: 7 }) + ".jpg";
-      var status = datStatus;
       var data = `${title},"${description}",${author_id},${published_year},${cover},${datStatus}\n`;
       if (i === 0) {
         writer.write(data, callback);
